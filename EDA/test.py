7099eaeb9815e2003d019588dd1ec7ae07e7e9e0
Mia Nakajima <MNakajima@TRCcompanies.com>
2021-10-25 22:23:24 -0700

    EDA continuation

--- EDA/test.py
+++ EDA/test.py
@@ -1,12 +1,6 @@
+import src.oasis_api as oa
 
-start_date = '20211001'
-end_date = '20211010'
-market_run_id = "DAM"
-node = "DLAP_PGAE-APND"
+test_pge = oa.get_LMP('20211001', '20211010')
 
-LMP_url = f"http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_LMP&" \
-          f"startdatetime={start_date}T07:00-0000&" \
-          f"enddatetime={end_date}T07:00-0000&version=1&" \
-          f"market_run_id={market_run_id}&node={node}"
-
-r = requests.get(LMP_url)
+print(test_pge.head())
+print(test_pge.dtypes)
