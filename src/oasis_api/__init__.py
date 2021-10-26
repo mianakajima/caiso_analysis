bcbcb95004a5fe88ccd15c60e776c0a595a6bcb5
Mia Nakajima <MNakajima@TRCcompanies.com>
2021-10-25 22:23:06 -0700

    Update API helper function, create helper function to read data in sql

--- src/oasis_api/__init__.py
+++ src/oasis_api/__init__.py
@@ -1,4 +1,5 @@
 from .oasis_api_helpers import test_func
 from .oasis_api_helpers import get_LMP
+from .oasis_api_helpers import import_to_mysql
 
-__all__ = ['test_func', 'get_LMP']
+__all__ = ['test_func', 'get_LMP', 'import_to_mysql']
--- src/oasis_api/oasis_api_helpers.py
+++ src/oasis_api/oasis_api_helpers.py
@@ -3,9 +3,11 @@ import zipfile
 import xml.etree.ElementTree as ET
 import pandas as pd
 import re
+import os
+import mysql.connector
 
 def test_func(x):
-    return x
+    return x*3
 
 def get_LMP(start_date, end_date, market_run_id = "DAM", node = "DLAP_PGAE-APND"):
     #start_date and end_date of format YYYYMMDD as a string
@@ -16,21 +18,29 @@ def get_LMP(start_date, end_date, market_run_id = "DAM", node = "DLAP_PGAE-APND"
 
     r = requests.get(LMP_url)
 
+    #change working directory to folder the script is in
+    abspath = os.path.abspath(__file__)
+    dname = os.path.dirname(abspath)
+    os.chdir(dname)
+    # print(os.getcwd())
+    # os.chdir('/Users/mnakajim/Documents/Github/caiso_analysis/src/oasis_api')
+
     #extract zipfile
-    with open('temp_zip.zip', 'wb') as code:
+    with open('./temp_zip.zip', 'wb') as code:
         code.write(r.content)
 
-    with zipfile.ZipFile('temp_zip', 'r') as zip_ref:
-        zip_ref.extractall('data') ##TODO: creating a data folder
+    with zipfile.ZipFile('./temp_zip.zip', 'r') as zip_ref:
+        zip_ref.extractall('./download_data')
+
 
     #get name of XML file
     header_filename = r.headers['Content-Disposition']
     zip_file = re.sub('inline; filename=', '', header_filename)
     xml_file = re.sub('.zip;', '.xml', zip_file)
-
+    os.remove('temp_zip.zip')
     #parse XML file
 
-    xml_data = ET.parse(f"data/{xml_file}")
+    xml_data = ET.parse(f"download_data/{xml_file}")
     root = xml_data.getroot()
 
     #extract values of interest
@@ -51,4 +61,37 @@ def get_LMP(start_date, end_date, market_run_id = "DAM", node = "DLAP_PGAE-APND"
                         'resource_name': resource_name,
                         'value': values})
 
+    #convert to correct data types
+    lmp_df[['interval_num', 'value']] = lmp_df[['interval_num', 'value']].apply(pd.to_numeric)
+    lmp_df[['opr_date', 'interval_start', 'interval_end']] = lmp_df[
+        ['opr_date', 'interval_start', 'interval_end']].apply(pd.to_datetime)
+
     return(lmp_df)
+
+
+#TODO: make this an object so I can close it?
+def import_to_mysql(lmp_df, user, password, host, database):
+    # connect to database
+    cnx = mysql.connector.connect(user= user , password= password,
+                                  host= host,
+                                  database= database,
+                                  auth_plugin='mysql_native_password')
+
+    cursor = cnx.cursor()
+
+    add_query = ("INSERT INTO lmp"
+                 "(interval_start, interval_end, interval_num, resource_name, value)"
+                 "VALUES (%s, %s, %s, %s, %s)")
+
+    data = lmp_df[['interval_start', 'interval_end', 'interval_num',
+                     'resource_name', 'value']].values.tolist()
+
+    print('Loading into SQL 1000 rows at a time... ')
+
+    for i in range(0, len(data), 1000):
+        print(str(i) + "/" + str(len(data)))
+        cursor.executemany(add_query, data[i: i + 1000])
+
+    cnx.commit()
+    cursor.close()
+    cnx.close()
